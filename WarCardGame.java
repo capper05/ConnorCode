a54c1b19552489457fde699956f586e581791ea3
capper05 <connor@prestonweb.com>
2021-02-05 18:19:44 -0600

    Update WarCardGame.java

--- WarCardGame.java
+++ WarCardGame.java
@@ -4,66 +4,67 @@ conclusion, while counting the number of battles.*/
 
 public class WarCardGame {
     private static class Player {       //Stores a player's deck, adds/removes cards
-        char[] playerdeck = new char[52];       //Cards in beginning of array, extra space filled by null
+        char[] playerDeck = new char[52];       //Cards in beginning of array, extra space filled by null
         int deckLength = 26;
-        public Player (char[] inputdeck) {      //Constructor
+        public Player (char[] inputDeck) {      //Constructor
             for (int i=0;i<=25;i++) {
-                playerdeck[i] = inputdeck[i];
-                playerdeck[26+i] = '\u0000';
+                playerDeck[i] = inputDeck[i];
+                playerDeck[26+i] = '\u0000';
             }
         }//Constructor
         public char drawCard() {            //Removes card from beginning of deck
-            char drawnCard = playerdeck[0]; //and moves each other card up one spot
+            char drawnCard = playerDeck[0]; //and moves each other card up one spot
             if (drawnCard == '\u0000') {
                 return drawnCard;
             }
             deckLength -= 1;
             for (int i=1;i<=51;i++) {
-                if (playerdeck[i] == '\u0000') {
-                    playerdeck[i-1] = '\u0000';
+                if (playerDeck[i] == '\u0000') {
+                    playerDeck[i-1] = '\u0000';
                     break;
                 }
-                playerdeck[i-1] = playerdeck[i];
+                playerDeck[i-1] = playerDeck[i];
             }
             return drawnCard;
         }//drawCard
+        public char[] getPlayerDeck() {
+            return playerDeck;
+        }
         public void recycleCard(char recycledCard) {  //Replaces first null slot with card
-            if(recycledCard != '\u0000') {
-                playerdeck[deckLength] = recycledCard;
-                deckLength += 1;
-            }
+            playerDeck[deckLength] = recycledCard;
+            deckLength += 1;
         }//recycleCard
     }//Player class
     public static class Game {                  //Contains methods to deal the cards, hold Battles and award cards to winners
-        char[] cards = {'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
-        char[] deck = new char[52];
-        char[][] dealtdecks = new char[2][26];
-        public void genDeck() {         //Creates deck of 52 cards in random order
+        static char[] cards = {'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
+        static char[] deck = new char[52];
+        static char[][] dealtDecks = new char[2][26];
+        public static void genDeck() {         //Creates deck of 52 cards in random order
             for (char c:cards) {
                 for (int i=0;i<=3;i++) {
                     while (true) {
-                        int randnum = (int)(Math.random()*52);
-                        if(deck[randnum] == '\u0000') {
-                            deck[randnum] = c;
+                        int randNum = (int)(Math.random()*52);
+                        if(deck[randNum] == '\u0000') {
+                            deck[randNum] = c;
                             break;
                         }
                     }
                 }      
             }   
         }//genDeck
-        public void dealCards() {       //Distributes cards to players, alternating
+        public static void dealCards() {       //Distributes cards to players, alternating
             for (int i=0;i<=51;i++) {
                 if (i%2 == 0) {
-                    dealtdecks[i%2][i/2] = deck[i];
+                    dealtDecks[i%2][i/2] = deck[i];
                 }else {
-                    dealtdecks[i%2][i/2] = deck[i];
+                    dealtDecks[i%2][i/2] = deck[i];
                 }
             }
         }//dealCards
-        public char[] getDeck(int decknum) {     //Retrieves one of two halves of the deck
-            return dealtdecks[decknum-1];
+        public static char[] getDeck(int decknum) {     //Retrieves one of two halves of the deck
+            return dealtDecks[decknum-1];
         }//getDeck
-        public char compareCards(char firstCard,char secondCard) {      //Compares 2 card values
+        public static char compareCards(char firstCard,char secondCard) {      //Compares 2 card values
             int firstCardNum=0;
             int secondCardNum=0;
             for (int i=0;i<= 12;i++) {
@@ -80,7 +81,7 @@ public class WarCardGame {
                 return secondCard;
             }
         }//compareCards
-        public void awardCards(Player player,char card,char otherCard,char[] pool) {//, char[] secondPool) {    //Gives cards from battle to victor
+        public static void awardCards(Player player,char card,char otherCard,char[] pool) {//, char[] secondPool) {    //Gives cards from battle to victor
             /**player.recycleCard(card);        //Procedural card return
             player.recycleCard(otherCard);
             for (char c:pool) {
@@ -108,28 +109,20 @@ public class WarCardGame {
             }
             countNum += 2;
             for (int i=cardList.length;i>0;i--) {
-                int randnum = (int)(Math.random()*i);
-                //System.out.println(cardList[randnum]);
-                player.recycleCard(cardList[randnum]);
-                for (int j = randnum;j<=cardList.length-2;j++) {
+                int randNum = (int)(Math.random()*i);
+                player.recycleCard(cardList[randNum]);
+                for (int j = randNum;j<=cardList.length-2;j++) {
                     cardList[j] = cardList[j+1];
                 }
             }
         }//awardCards
-        public void battle(Player firstPlayer, Player secondPlayer) {   //Compare cards, give them to victor
+        public static void battle(Player firstPlayer, Player secondPlayer) {   //Compare cards, give them to victor
             char card1 = firstPlayer.drawCard();
             char card2 = secondPlayer.drawCard();
-            if (card1 == '\u0000' || card2 == '\u0000') {
-                return;
-            }
-            char[] pool1 = new char[52];
-            int pool1Length = 0;
-            char[] pool2 = new char[52];
             char[] pool = new char[52];
             int poolLength = 0;
-            int pool2Length = 0;
-            char drawnCard3='\u0000';
-            char drawnCard4='\u0000';
+            char drawnCard1='\u0000';
+            char drawnCard2='\u0000';
             while (card1 == card2) {           //Deals with a tie AKA "War"
                 System.out.println("War");
                 pool[poolLength] = card1;
@@ -138,12 +131,12 @@ public class WarCardGame {
                 pool[poolLength] = firstPlayer.drawCard();
                 pool[poolLength+1] = secondPlayer.drawCard();
                 poolLength += 2;
-                drawnCard3 = firstPlayer.drawCard();
-                drawnCard4 = secondPlayer.drawCard();
+                drawnCard1 = firstPlayer.drawCard();
+                drawnCard2 = secondPlayer.drawCard();
                 
-                if (pool[poolLength-1] == '\u0000' || drawnCard3 == '\u0000') { //if player1 can't draw card
-                    pool[poolLength] = drawnCard3;
-                    pool[poolLength+1] = drawnCard4;
+                if (pool[poolLength-1] == '\u0000' || drawnCard1 == '\u0000') { //if player1 can't draw card
+                    pool[poolLength] = drawnCard1;
+                    pool[poolLength+1] = drawnCard2;
                     poolLength += 2;
                     for (int i=0;i<poolLength;i++) {
                         if (pool[i] != '\u0000') {
@@ -152,9 +145,9 @@ public class WarCardGame {
                     }
                     return;
                 }
-                if (pool[poolLength-1] == '\u0000' || drawnCard4 == '\u0000') {
-                    pool[poolLength] = drawnCard3;
-                    pool[poolLength+1] = drawnCard4;
+                if (pool[poolLength-1] == '\u0000' || drawnCard2 == '\u0000') {
+                    pool[poolLength] = drawnCard1;
+                    pool[poolLength+1] = drawnCard2;
                     poolLength += 2;
                     for (int i=0;i<poolLength;i++) {
                         if (pool[i] != '\u0000') {
@@ -163,60 +156,54 @@ public class WarCardGame {
                     }
                     return;
                 }
-                card1 = drawnCard3;
-                card2 = drawnCard4;
-               }
+                card1 = drawnCard1;
+                card2 = drawnCard2;
+            }
 
             if (compareCards(card1,card2) == card1) {
                 awardCards(firstPlayer,card1,card2,pool);   //Gives cards to player 1
             }else {
                 awardCards(secondPlayer,card1,card2,pool);  //Gives cards to player 2
             }
-        showScore(firstPlayer,secondPlayer);
+            showScore(firstPlayer,secondPlayer);
         }//battle
-        public void showScore(Player playerOne,Player playerTwo) {         //Displays each player's deck
-            for (char c:playerOne.playerdeck) {
+        public static void showScore(Player playerOne,Player playerTwo) {         //Displays each player's deck
+            for (char c:playerOne.getPlayerDeck()) {
                 if (c != '\u0000') {
                     System.out.print(c);
                 }
             }
             System.out.print("          ");
-            for (char c:playerTwo.playerdeck) {
+            for (char c:playerTwo.getPlayerDeck()) {
                 if (c != '\u0000') {
                     System.out.print(c);
                 }
             }
             System.out.println("");
-        }
-        public String checkWinner(Player firstPlayer, Player secondPlayer) {    //Checks if player has 52 cards
-            if (!(firstPlayer.playerdeck[51] == '\u0000')) {
+        }//showScore
+        public static String checkWinner(Player firstPlayer, Player secondPlayer) {    //Checks if player has 52 cards
+            if (!(firstPlayer.getPlayerDeck()[51] == '\u0000')) {
                 return "Player 1";
-            }else if (!(secondPlayer.playerdeck[51] == '\u0000')) {
+            }else if (!(secondPlayer.getPlayerDeck()[51] == '\u0000')) {
                 return "Player 2";
             }
             return "";
         }//checkWinner
         
-    }
+    }//Game
     public static void main(String[] args) {
-        Game game1 = new Game();
-        game1.genDeck();
-        game1.dealCards();
+        Game.genDeck();
+        Game.dealCards();
         String check = "";
-
-        Player player1 = new Player(game1.getDeck(1));
-        Player player2 = new Player(game1.getDeck(2));
-
+        Player player1 = new Player(Game.getDeck(1));
+        Player player2 = new Player(Game.getDeck(2));
         int count = 0;
         while (check == ""){          //loops through battles, checks winner
             count += 1;
-            game1.battle(player1,player2);
-            check = game1.checkWinner(player1,player2);
-            if (check != "") {
-                System.out.println(""+check+" wins!");
-            }
+            Game.battle(player1,player2);
+            check = Game.checkWinner(player1,player2);;
         }
+        System.out.println(""+check+" wins!");
         System.out.println(count+" battles.");
-        
     }//main
 }//WarSim
